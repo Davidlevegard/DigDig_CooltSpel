45c34e7604c500159fe9b6db56d51d84476ff5ab
Anktratten <rasmus.kackeliasson@elev.ga.lbs.se>
2021-12-13 10:57:29 +0100

    Health Script

--- D R A DigDig Monster Dödar Simulator (COOL)/Assets/Script/Health.cs
+++ D R A DigDig Monster Dödar Simulator (COOL)/Assets/Script/Health.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnkScript : MonoBehaviour
+public class Health : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,6 @@ public class AnkScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(inga ankor)
-                {
-            skapa en jävla anka
-                }
+        
     }
 }
