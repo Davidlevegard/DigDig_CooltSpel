e46efcb56e08441f8bc659164fbf357bbf8ecd00
Davidlevegard <david.levegard@elev.ga.lbs.se>
2021-12-13 12:11:35 +0100

    stuff

---
+++ D R A DigDig Monster Dödar Simulator (COOL)/Assets/Script/DevilDamage.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevilDamage : MonoBehaviour
+{
+    public HealthbarController healthBar;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
